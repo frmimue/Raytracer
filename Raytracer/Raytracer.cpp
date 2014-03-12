eebf6fef4ed03d846e239376c8486307141b4f90
Frank Mueller <frmimue@gmail.com>
2014-03-12 14:47:05 +0100

    Lights can now be loaded from scene.txt

--- Raytracer/Raytracer.cpp
+++ Raytracer/Raytracer.cpp
@@ -14,8 +14,8 @@
 
 int main(int argv, char **argc) {
 
-	int testX = 800, testY = 600;
-	Camera testCamera = Camera(Vector3D(0, -5, 1), Vector3D(0, 1, 0));
+	int testX = 1920, testY = 1080;
+	Camera testCamera = Camera(Vector3D(0, -8, 1.5), Vector3D(0, 1, 0));
 	Scene *scene = new Scene();
 
 	std::ifstream sceneFile("scene.txt");
@@ -27,25 +27,32 @@ int main(int argv, char **argc) {
 
 	JsonGroup json(s);
 	JsonArray objects(json.at("objects").Array());
+	JsonArray lights(json.at("lights").Array());
 
 	for (int i = 0; i < objects.size(); i++){
 		JsonGroup object = objects.at(i).Group();
 
 		if (object.at("type").String().compare("sphere") == 0){
 			scene->add(new SphereObject(Vector3D(object.at("position").Array().at(0).Float(), object.at("position").Array().at(1).Float(), object.at("position").Array().at(2).Float()),
-				object.at("radius").Float(), Color(object.at("color").Array().at(0).Integer(), object.at("color").Array().at(1).Integer(), object.at("color").Array().at(2).Integer())));
+				object.at("radius").Float(), Color(object.at("color").Array().at(0).Integer(), object.at("color").Array().at(1).Integer(), object.at("color").Array().at(2).Integer()), object.at("reflection").Float()));
 		}
-		
+
 		if (object.at("type").String().compare("plane") == 0){
 			scene->add(new PlaneObject(Vector3D(object.at("position").Array().at(0).Float(), object.at("position").Array().at(1).Float(), object.at("position").Array().at(2).Float()),
 				Vector3D(object.at("normal").Array().at(0).Float(), object.at("normal").Array().at(1).Float(), object.at("normal").Array().at(2).Float()),
 				Color(object.at("color").Array().at(0).Array().at(0).Integer(), object.at("color").Array().at(0).Array().at(1).Integer(), object.at("color").Array().at(0).Array().at(2).Integer()),
-				Color(object.at("color").Array().at(1).Array().at(0).Integer(), object.at("color").Array().at(1).Array().at(1).Integer(), object.at("color").Array().at(1).Array().at(2).Integer())));
+				Color(object.at("color").Array().at(1).Array().at(0).Integer(), object.at("color").Array().at(1).Array().at(1).Integer(), object.at("color").Array().at(1).Array().at(2).Integer()), object.at("reflection").Float()));
 		}
 	}
 
-    scene->add(new Light(Vector3D(2, 1, 1)));
-    scene->add(new Light(Vector3D(-2, 1, 1)));
+	for (int i = 0; i < lights.size(); i++){
+		JsonGroup light = lights.at(i).Group();
+
+		if (light.at("type").String().compare("point") == 0){
+			scene->add(new Light(Vector3D(light.at("position").Array().at(0).Float(), light.at("position").Array().at(1).Float(), light.at("position").Array().at(2).Float())));
+		}
+	}
+
     std::cout << std::endl << "Creating test.ppm" << std::endl;
     PixelBuffer *testBuffer = new PixelBuffer(testX, testY);
     PixelBuffer *testBuffer2 = new PixelBuffer(testX, testY);
